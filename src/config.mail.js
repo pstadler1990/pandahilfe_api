59a8da39c35ec784a394ba44d926941923c27178
pstadler1990 <pstadler90@gmx.de>
2020-03-17 23:20:18 +0100

    added mail support

---
+++ src/config.mail.js
@@ -0,0 +1,8 @@
+const configMail = Object.freeze({
+    user: '<removed>',
+    password: '<removed>',
+    sender: '<removed>',
+    deleteReceivers: ['<removed>']
+});
+
+module.exports = configMail;
